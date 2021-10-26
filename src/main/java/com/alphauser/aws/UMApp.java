4c367306fcd201d3a82600a06285ef93978ee99e
kumar24 <racherlasarath24@gmail.com>
2021-10-26 16:46:31 +0530

    Commit # AWS EKS with Springboot App
    Commit # AWS EKS with Springboot App

---
+++ src/main/java/com/alphauser/aws/UMApp.java
@@ -0,0 +1,13 @@
+package com.alphauser.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UMApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UMApp.class, args);
+	}
+
+}
